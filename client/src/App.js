8006ead50f54b986b5fd2dd67336796a83c26db1
Seth <sethvance26@gmail.com>
2021-09-21 19:18:55 -0400

    adding a navbar component, deleting other things

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-  
+      <Navbar/>
+      <h1>App</h1>
     </div>
   );
 }
