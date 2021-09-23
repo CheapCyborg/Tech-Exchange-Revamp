ad87b09567bd34f4107647e152350f94f0dcb33f
Damian Green <damiang58@gmail.com>
2021-09-23 16:12:31 -0400

    typescript config and set up

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-import consoleRoutes from './routes/consoles.js';
+import consoleRoutes from './routes/consoles';
 
 const app = express();
 dotenv.config();
@@ -15,9 +15,9 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json()); // To parse the incoming requests with JSON payloads
 app.use(cors());
 
-const CONNECTION_URL = process.env.CONNECTION_URL;
+const CONNECTION_URL:string = process.env.CONNECTION_URL!;
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
