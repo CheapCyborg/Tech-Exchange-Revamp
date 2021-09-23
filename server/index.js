e043688683fd0f14eebe9d49fa5b10664d73a279
Damian Green <damiang58@gmail.com>
2021-09-23 10:48:34 -0400

    removing secrets

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import consoleRoutes from './routes/consoles.js';
 
 const app = express();
+dotenv.config();
 
 app.use('/consoles', consoleRoutes);
 
@@ -13,7 +15,7 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json()); // To parse the incoming requests with JSON payloads
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://cheapcyborg:1234567q@cluster0.1ddfw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
